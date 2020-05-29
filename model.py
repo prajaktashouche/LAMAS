0fb3cd12e8f77d90ad2ca02d4413494cb488eb01
prajaktashouche <prajakta.90199@gmail.com>
2020-05-29 16:54:22 +0200

    generate all possible worlds (incomplete)

--- model.py
+++ model.py
@@ -1,22 +1,33 @@
 from mlsolver.kripke import World, KripkeStructure
 import itertools
 
+import numpy as np
+from collections import Counter
+
 
 def kripke_model():
 
-    # Currently, a trial just for Aces:
-    # Generate all possible combinations of dealing Aces amongst the three players
-    value = [True, False]
-    possibilities = list(itertools.product(value, repeat=3))
+    deck = ['A', 'A', '2', '2', '3', '3']
 
-    worlds = []
-    for i, p in enumerate(possibilities):
-        if i != 1:  # Skip True, True, True since there are only two aces
-            v1, v2, v3 = p
-            worlds.append(World(str(i), {'Ace_1': v1, 'Ace_2': v2, 'Ace_3': v3}))
+    # A trial for generating all possible worlds (can be made more efficient)
+    combinations = set(list(itertools.combinations(deck, 2)))
+    possible = set(list(itertools.product(combinations, repeat=3)))
 
-    print(worlds)
+    # Currently, there are duplicates such as (2,3) and (3,2) are treated as different
+    possible_worlds = []
+    for p in possible:
+        # Count the occurrence of each card in the combination
+        count = [0, 0, 0]
+        for i, card in enumerate(set(deck)):
+            count[i] += Counter(j[0] for j in list(p))[card] + Counter(j[1] for j in list(p))[card]
 
+        # Only keep the combinations were the total count of each card is exactly 2
+        if not any(c != 2 for c in count):
+            possible_worlds.append(p)
+
+    possible_worlds = set(possible_worlds)
+
+    # Make kripke model of the possible worlds
 
 def main():
     kripke_model()
