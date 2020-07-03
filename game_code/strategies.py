0133acf4d6479c4c4570649b80dd3396a4376a01
shaniyahali <s.hassan.ali@student.rug.nl>
2020-07-03 17:22:29 +0200

    run 500 simulations, update results page

--- game_code/strategies.py
+++ game_code/strategies.py
@@ -1,6 +1,7 @@
 from itertools import product
 import random
 import time
+import os
 from game import Game
 
 
@@ -13,6 +14,8 @@ class Strategy:
 
         self.strategies = {}
         self.get_strategies()
+
+        self.stats = {}
 
     def get_strategies(self):
         strategies = list(product(self.player_ids, self.announcement))
@@ -36,15 +39,15 @@ class Strategy:
             announcement = "PLACE"
             player_hand = players[player_idx].get_hand_values()
             if card_to_place in player_hand:
-                game.player_announcement(player_idx, card_to_place, "PLACE", True)
-                turns.append(["PLACE", True])
+                call_bluff = game.player_announcement(player_idx, card_to_place, "PLACE", True)
+                turns.append([player_idx, "PLACE", True, call_bluff])
             else:
                 random_strategy = random.choice(self.strategies[player_idx])
                 [announcement, statement] = random_strategy
 
                 random_card = random.choice(player_hand) if announcement == "PLACE" else None
-                game.player_announcement(player_idx, card_to_place, announcement, statement, random_card)
-                turns.append([announcement, statement])
+                call_bluff = game.player_announcement(player_idx, card_to_place, announcement, statement, random_card)
+                turns.append([player_idx, announcement, statement, call_bluff])
 
             # reset player id
             player_idx += 1
@@ -66,23 +69,86 @@ class Strategy:
 
         return ret_player, turns
 
+    @staticmethod
+    def write_to_file(plays):
+        filename = "./strategies.txt"
+
+        if os.path.exists(filename):
+            os.remove(filename)
+
+        with open(filename, 'a+') as file:
+            for key, val in plays.items():
+                file.write("\n")
+                file.write("------PLAYER: {}".format(key))
+                for item in val:
+                    file.write("\n")
+                    file.write("{}".format(item))
+
+    @staticmethod
+    def print_time():
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        print(current_time)
+
     def simulate_multiple_plays(self, deal_type):
-        runs = 20
+        runs = 100
         start = time.time()
-        stats = {}
+        self.print_time()
+
+        self.stats = {}
         for i in range(runs):
             game = Game(deal_type)
             player, play = self.get_plays(game)
-            if player in stats:
-                stats[player].append(play)
+            if player in self.stats:
+                self.stats[player].append(play)
             else:
-                stats[player] = [play]
+                self.stats[player] = [play]
 
-        for key, val in stats.items():
-            print(key, len(val))
+        self.write_to_file(self.stats)
 
+        self.print_time()
         print('It took', time.time() - start, 'seconds.')
 
+        self.get_stats()
+
+    def get_stats(self):
+
+        # get stats for getting called on bluff
+        bluff_called_stats = {}
+        for key, val in self.stats.items():
+            counter = 0
+            for play in val:
+                # check last play was called a bluff - loses game
+                strat = play[-1]
+                if strat[2] is False and strat[3] is True:
+                    counter += 1
+            bluff_called_stats[key] = counter / len(val)
+
+        # get stats for getting away with bluff
+        bluff_stats = {}
+        for key, val in self.stats.items():
+            counter = 0
+            for play in val:
+                # check last play
+                strat = play[-1]
+                if strat[3] is False:
+                    player_play = [x for x in play if x[0] == key and x[2] is False]
+                    if len(player_play) > 0:
+                        counter += 1
+
+            bluff_stats[key] = counter / len(val)
+
+        print("Winner ratio---")
+        for key, val in self.stats.items():
+            print(key, len(val))
+
+        print("Getting away with bluffing---")
+        for key, val in bluff_stats.items():
+            print(key, val * 100)
+
+        print("Getting called on their bluff---")
+        for key, val in bluff_called_stats.items():
+            print(key, val * 100)
 
 strat = Strategy()
-strat.simulate_multiple_plays(1)
+strat.simulate_multiple_plays(2)
